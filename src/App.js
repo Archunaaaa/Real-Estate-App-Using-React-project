fe3983658016ddd543e00d48713a947a375ea0bf
archunar <you@example.com>
2024-03-27 17:17:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
-import React from "react"
-import Header from "../Common/Header/Header"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Home from "../Home/Home"
-import Footer from "../Common/Footer/Footer"
-import About from "../About/About"
-import Pricing from "../Pricing/Pricing"
-import Blog from "../Blog/Blog"
-import Services from "../Services/Services"
-import Contact from "../Contact/Contact"
+import "./App.css"
+import Pages from "./Components/Pages/Pages"
 
-const Pages = () => {
-  return (
-    <>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/services' component={Services} />
-          <Route exact path='/blog' component={Blog} />
-          <Route exact path='/pricing' component={Pricing} />
-          <Route exact path='/contact' component={Contact} />
-        </Switch>
-        <Footer />
-      </Router>
-    </>
-  )
+function App() {
+  return <Pages />
 }
-export default Pages
 
-
+export default App
